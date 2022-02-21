a25edea6b5205949427501f298611730e2c7167c
devyani143 <82705704+devyani143@users.noreply.github.com>
2022-02-22 00:01:51 +0530

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,11 @@
+# dev=input("enter a alphabet")
+# if dev>="a" and dev<="z":
+#     print("small latters")
+#     capital=input("enter the capital")
+#     if capital>="A" and capital<="Z":
+#         print("capital latters")
+#     else:
+#         print("not capital")
+# else:
+#     print("not small")
+    
